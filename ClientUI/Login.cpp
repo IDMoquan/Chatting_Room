22ada13769c521929df116625fbb07870137d24c
FOLINK <108606294+IDMoquan@users.noreply.github.com>
2024-12-01 18:33:07 +0800

    圆角

--- ClientUI/Login.cpp
+++ ClientUI/Login.cpp
@@ -9,6 +9,16 @@ Login::Login(QWidget *parent)
 {
 	ui.setupUi(this);
 	connect(ui.pushButton, SIGNAL(clicked()), this, SLOT(login()));
+	ui.lineEdit->setStyleSheet("font: 25 14pt '微软雅黑 Light';" //字体
+		"color: rgb(31,31,31);"		//字体颜色
+		"padding-left:20px;"       //内边距-字体缩进
+		"background-color: rgb(255, 255, 255);" //背景颜色
+		"border:2px solid rgb(20,196,188);border-radius:15px;");//边框粗细-颜色-圆角设置 
+	ui.lineEdit_2->setStyleSheet("font: 25 14pt '微软雅黑 Light';" //字体
+		"color: rgb(31,31,31);"		//字体颜色
+		"padding-left:20px;"       //内边距-字体缩进
+		"background-color: rgb(255, 255, 255);" //背景颜色
+		"border:2px solid rgb(20,196,188);border-radius:15px;");//边框粗细-颜色-圆角设置 
 }
 
 Login::~Login()
@@ -40,11 +50,7 @@ void Login::login() {
 	username = gs_username.c_str();
 	password = gs_password.c_str();
 	//界面美化
-	ui.lineEdit->setStyleSheet("font: 25 14pt '微软雅黑 Light';" //字体
-		"color: rgb(31,31,31);"		//字体颜色
-		"padding-left:20px;"       //内边距-字体缩进
-		"background-color: rgb(255, 255, 255);" //背景颜色
-		"border:2px solid rgb(20,196,188);border-radius:15px;");//边框粗细-颜色-圆角设置 
+	
 	//发送用户名密码
 	::send(client_socket, username, 1024, 0);
 	::send(client_socket, password, 1024, 0);
